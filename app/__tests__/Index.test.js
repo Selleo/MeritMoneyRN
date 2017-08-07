26069042c6e49c64a5c0be67580f53d088c0fc6b
dpienczykowski <dpienczykowski@gmail.com>
2017-08-07 21:07:55 +0200

    split index file into app folder, add babel and resolve src

--- app/__tests__/Index.test.js
+++ app/__tests__/Index.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import App from './App';
+import Index from '../index';
 
 import renderer from 'react-test-renderer';
 
 it('renders without crashing', () => {
-  const rendered = renderer.create(<App />).toJSON();
+  const rendered = renderer.create(<Index />).toJSON();
   expect(rendered).toBeTruthy();
+  expect(rendered).toMatchSnapshot();
 });
