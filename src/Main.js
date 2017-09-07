631f34917367722d6d2e617115dc3f0e6d83575b
Damian Sznajder <damsznajder@gmail.com>
2017-09-07 22:04:34 +0200

    Handle user auth cancel

--- src/Main.js
+++ src/Main.js
@@ -1,33 +1,57 @@
 import React, { Component } from 'react'
-import { AsyncStorage, Text, View } from 'react-native'
+import { Text, View } from 'react-native'
 import Auth0 from 'react-native-auth0'
-import { CLIENT_ID, DOMAIN_URL } from 'react-native-dotenv'
+import { Button, Rating } from 'react-native-elements'
 import jwtDecoder from 'jwt-decode'
+import { CLIENT_ID, DOMAIN_URL } from 'react-native-dotenv'
 
 export default class Main extends Component {
-  componentWillMount = () => {
-    if (!AsyncStorage.getItem('credentials')) {
-      const auth0 = new Auth0({
-        domain: `https://${DOMAIN_URL}`,
-        clientId: CLIENT_ID,
-      })
-
-      auth0
-        .webAuth
-        .authorize({
-          audience: `https://${DOMAIN_URL}/userinfo`,
-          scope: 'openid email',
-        })
-        .then(creds => {
-          AsyncStorage.setItem('credentials', jwtDecoder(creds))
-        })
-    }
+  state = {
+    loading: true,
   }
 
+  authorize = async () => {
+    let auth0 = new Auth0({
+      domain: `https://${DOMAIN_URL}`,
+      clientId: CLIENT_ID,
+    })
+
+    auth0
+      .webAuth
+      .authorize({
+        audience: `https://${DOMAIN_URL}/userinfo`,
+        scope: 'openid email profile',
+      })
+      .then(({ idToken }) => {
+        this.setState({ userInfo: jwtDecoder(idToken) })
+      })
+    }
+
   render() {
+    const { userInfo } = this.state
+
+    if (!userInfo) {
+      return (
+        <View>
+          <Button
+            onPress={this.authorize}
+            testID="main"
+            title='Loading'
+          />
+        </View>
+      )
+    }
+
     return (
       <View>
         <Text testID="main"> MeritMoney </Text>
+        <Rating
+          imageSize={40}
+          onFinishRating={() => {}}
+          showRating
+          startingValue={1}
+          type="star"
+        />
       </View>
     )
   }
