80402ccc4096a8d9fcd40c86b1cafababceaec9f
kantorm <kantorm96@gmail.com>
2017-09-11 20:52:40 +0200

    Hamsters component

--- src/CollectorsAndHamsters.js
+++ src/CollectorsAndHamsters.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react'
 import { Text, View, StyleSheet } from 'react-native'
+import { Divider } from 'react-native-elements'
 
-import Collectors from './Collectors';
+import Collectors from './Collectors'
+import Hamsters from './Hamsters'
+
 export default class CollectorsAndHamsters extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>CollectorsAndHamsters</Text>
+        <View style={styles.headerContainer}>
+          <Text style={styles.header}>TOP Collectors</Text>
+        </View>
         <Collectors />
+        <Divider style={styles.divider}/>
+        <View style={styles.headerContainer}>
+          <Text style={styles.header}>Hamsters</Text>
+        </View>
+        <Hamsters />
       </View>
     )
   }
@@ -16,7 +26,18 @@ export default class CollectorsAndHamsters extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    // justifyContent: 'center',
-    // alignItems: 'center',
-  }
+  },
+  headerContainer: {
+    marginTop: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  header: {
+    fontSize: 26,
+  },
+  divider: {
+    backgroundColor: 'blue',
+    height: 1,
+    margin: 10,
+  },
 })
---
+++ src/Hamsters.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react'
+import { View, Image, StyleSheet } from 'react-native'
+import { Avatar } from 'react-native-elements'
+
+export default class Hamsters extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.hamstersContainer}>
+          <Avatar medium rounded source={{uri: 'https://www.javascript.ba/images/logos/framework/EmberJS.jpg'}} />
+          <Avatar medium rounded source={{uri: 'https://www.javascript.ba/images/logos/framework/EmberJS.jpg'}} />
+          <Avatar medium rounded source={{uri: 'https://www.javascript.ba/images/logos/framework/EmberJS.jpg'}} />
+          <Avatar medium rounded source={{uri: 'https://www.javascript.ba/images/logos/framework/EmberJS.jpg'}} />
+          <Avatar medium rounded source={{uri: 'https://www.javascript.ba/images/logos/framework/EmberJS.jpg'}} />
+          <Avatar medium rounded source={{uri: 'https://www.javascript.ba/images/logos/framework/EmberJS.jpg'}} />
+          <Avatar medium rounded source={{uri: 'https://www.javascript.ba/images/logos/framework/EmberJS.jpg'}} />
+          <Avatar medium rounded source={{uri: 'https://www.javascript.ba/images/logos/framework/EmberJS.jpg'}} />
+        </View>
+        <View style={styles.imageContainer}>
+          <Image
+            source={{uri: 'https://www.emberjs.com/images/tomsters/teaching-reverse-79f66d70.png'}}
+            style={styles.image}
+          />
+        </View>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 0.5,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  imageContainer: {
+    alignItems: 'flex-end',
+  },
+  hamstersContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    width: 200,
+    margin: 10,
+  },
+  image: {
+    width: 100,
+    height: 100,
+  },
+})
