0d733ed53b80f19ee065454100f2b37078bf399b
kantorm <kantorm96@gmail.com>
2017-09-11 20:02:25 +0200

    Collectors component idea

---
+++ src/Collectors.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react'
+import { StyleSheet, View } from 'react-native'
+import { Avatar } from 'react-native-elements'
+
+export default class Collectors extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.firstPlace}>
+          <Avatar medium source={{uri: 'https://www.wykop.pl/cdn/c3201142/comment_lD8RanwxW9vxI4DNHXjEB2LLFh7wmnYk.jpg'}} />
+        </View>
+        <View style={styles.secondPlace}>
+          <Avatar medium source={{uri: 'https://www.wykop.pl/cdn/c3201142/comment_lD8RanwxW9vxI4DNHXjEB2LLFh7wmnYk.jpg'}} />
+        </View>
+        <View style={styles.thirdPlace}>
+          <Avatar medium source={{uri: 'https://www.wykop.pl/cdn/c3201142/comment_lD8RanwxW9vxI4DNHXjEB2LLFh7wmnYk.jpg'}} />
+        </View>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 0.5,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  },
+  thirdPlace: {
+    height: 100,
+    width: 50,
+    backgroundColor: '#888',
+    alignSelf: 'flex-end'
+  },
+  secondPlace: {
+    height: 140,
+    width: 50,
+    backgroundColor: '#888',
+    alignSelf: 'flex-end'
+  },
+  firstPlace: {
+    height: 180,
+    width: 50,
+    backgroundColor: '#888',
+    alignSelf: 'flex-end'
+  }
+})
