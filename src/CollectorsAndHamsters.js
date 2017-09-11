76fc9b561530fa2f4804dbe46afc2680609fb02e
kantorm <kantorm96@gmail.com>
2017-09-11 20:02:25 +0200

    Collectors component idea

--- src/CollectorsAndHamsters.js
+++ src/CollectorsAndHamsters.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import { Text, View, StyleSheet } from 'react-native'
 
+import Collectors from './Collectors';
 export default class CollectorsAndHamsters extends Component {
   render() {
     return (
       <View style={styles.container}>
         <Text>CollectorsAndHamsters</Text>
+        <Collectors />
       </View>
     )
   }
@@ -14,7 +16,7 @@ export default class CollectorsAndHamsters extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    // justifyContent: 'center',
+    // alignItems: 'center',
   }
 })
