1d8c0bef1d8a8e83b5a85e96f2c9031542f69d3d
Damian Sznajder <damsznajder@gmail.com>
2017-09-18 15:03:17 +0200

    Prestyled Component List

---
+++ src/store/comments/index.js
@@ -0,0 +1,6 @@
+import * as types from './constants'
+import * as actions from './actions'
+import reducer from './reducer'
+
+export { actions, types }
+export default reducer
---
+++ src/store/comments/reducer.js
@@ -0,0 +1,16 @@
+import { types } from './'
+
+const initialState = {
+
+}
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+
+  case types.LOAD_COMMENTS:
+    return action.payload
+
+  default:
+    return state
+  }
+}
