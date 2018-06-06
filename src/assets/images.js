51694077ce865d13da0f9582edcd5ca1ad98b746
Damian Sznajder <damsznajder@gmail.com>
2018-06-06 02:12:24 +0200

    Create custom Tabbar

---
+++ src/assets/images.js
@@ -0,0 +1,6 @@
+export default {
+  bubbleText: require('./bubble-text.png'),
+  home: require('./home.png'),
+  trophy: require('./trophy.png'),
+  user: require('./user.png'),
+}
---
+++ src/styles/colors.js
@@ -0,0 +1,3 @@
+export const primary = '#222F3F'
+export const secondary = '#30B97E'
+export const tab = '#3C6273'
