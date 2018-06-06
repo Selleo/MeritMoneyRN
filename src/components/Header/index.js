6e1a2df1b280b00550a09da790346a6482d43e59
Michał Kantor <kantorm96@gmail.com>
2018-06-06 19:57:21 +0200

    Header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react'
+import { StyleSheet, View, Text } from 'react-native'
+import { white, secondaryLight, primary } from 'src/styles/colors'
+import Icon from 'src/components/Icon'
+import LinearGradient from 'react-native-linear-gradient'
+
+export default class Header extends Component {
+  render() {
+    return (
+      <LinearGradient
+        colors={[secondaryLight, primary]}
+        end={{ x: 1, y: 1 }}
+        locations={[0, 1]}
+        start={{ x: 0, y: 1 }}
+      >
+        <View style={styles.headerContainer}>
+          <View style={styles.textContainer}>
+            <Icon name="star-empty3" size={18} style={styles.star} />
+            <Text style={styles.text}>15 left</Text>
+          </View>
+          <View style={styles.textContainer}>
+            <Text style={styles.text}>24(54)</Text>
+            <Text>^</Text>
+          </View>
+        </View>
+      </LinearGradient>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  headerContainer: {
+    paddingVertical: 25,
+    paddingHorizontal: 20,
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  text: {
+    fontSize: 16,
+    color: white,
+  },
+  textContainer: {
+    flexDirection: 'row',
+  },
+  star: {
+    color: white,
+    paddingHorizontal: 5,
+  },
+})
